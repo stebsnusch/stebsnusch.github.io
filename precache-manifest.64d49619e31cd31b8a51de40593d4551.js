4c5a46363adcc8fae204cf296af75afcdfa92cce
Stephany Nusch <stephany.nusch@quintoandar.com.br>
2020-08-09 14:05:31 -0300

    Updates

---
+++ precache-manifest.64d49619e31cd31b8a51de40593d4551.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "680eda25bdb5c510f75062e6f7c2c5ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fa7a1efa9d989993824",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fcfb521037f113f9dd7b",
+    "url": "/static/js/2.24f90ae3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.24f90ae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fa7a1efa9d989993824",
+    "url": "/static/js/main.4057e0ba.chunk.js"
+  },
+  {
+    "revision": "f7a508fabedf8090b50b",
+    "url": "/static/js/runtime-main.16702e51.js"
+  },
+  {
+    "revision": "0927d6fc70926c188baedfd170bd20b9",
+    "url": "/static/media/profile_pic.0927d6fc.png"
+  }
+]);
