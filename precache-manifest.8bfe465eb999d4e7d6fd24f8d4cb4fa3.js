8824774d742c5fcc38f4101d6ec741fbad888ecd
Stephany Nusch <stephany.nusch@quintoandar.com.br>
2020-11-13 22:25:37 -0300

    Updates

---
+++ precache-manifest.8bfe465eb999d4e7d6fd24f8d4cb4fa3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7e08fbeb5b03bb23ff856ff9f765982",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb298b83c89d767548bb",
+    "url": "/static/css/main.7dfe5454.chunk.css"
+  },
+  {
+    "revision": "9e26460bb82e112f85c2",
+    "url": "/static/js/2.89825550.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.89825550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb298b83c89d767548bb",
+    "url": "/static/js/main.bc0b0dc2.chunk.js"
+  },
+  {
+    "revision": "f7a508fabedf8090b50b",
+    "url": "/static/js/runtime-main.16702e51.js"
+  },
+  {
+    "revision": "e3c72916c07e365afdfa20aa39b125a0",
+    "url": "/static/media/STEPHANY_NUSCH_RESUME.e3c72916.pdf"
+  },
+  {
+    "revision": "fe1510d8ec4e16070ca4fb49ae7a55f5",
+    "url": "/static/media/carddeck.fe1510d8.png"
+  },
+  {
+    "revision": "cdeec0c5b641c5a5990cdeb7625da17c",
+    "url": "/static/media/minesweeper.cdeec0c5.png"
+  },
+  {
+    "revision": "5d73eb99cfb6d9fc462703496ba68ff7",
+    "url": "/static/media/portfolio.5d73eb99.png"
+  },
+  {
+    "revision": "f38f9106b96778a74e1c128f7725c87d",
+    "url": "/static/media/profile_pic.f38f9106.jpg"
+  }
+]);
